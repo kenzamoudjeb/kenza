e5577e313468c22cec6344ff6afa345030fbe6c9
kenzamoudjeb <84639773+kenzamoudjeb@users.noreply.github.com>
2021-06-07 21:21:27 +0100

    Update rdist1.R

--- R/rdist1.R
+++ R/rdist1.R
@@ -1,7 +1,7 @@
 #'simulation k fois
 #'@export
 #'@param x numeric vector representing the values of the random variable
-#'@param p numeric vector repesenting the probabilities
+#'@param p numeric vector representing the probabilities
 #'@param k numeric
 
 rdistk<-function(x,p,k)
