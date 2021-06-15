98de894baf85163c4567afa7d37f598cf7404113
kenzamoudjeb <84639773+kenzamoudjeb@users.noreply.github.com>
2021-06-15 05:26:30 +0100

    Update simul2.R

--- R/simul2.R
+++ R/simul2.R
@@ -6,16 +6,16 @@
 #´@param n numeric number representing number of steps
 
 
-Sim<-function(l,ck,P,n)
+Simul2<-function(l,mu,P,n)
 {
   y<-c(rep(0,n+1))
   t<-c(0:n)
-  y[1]<-rdist(l,ck)
+  y[1]<-rdist(l,mu)
   for(i in 1:n){
     y[i+1]<-rdist(l,P[y[i],])
   }
   
-  plot(t,y,type = "b",main = "graphe des états " ,pch=5,xlim=c(0,n),ylim=c(0,length(ck)+1),xlab = "temps" ,ylab = "états",col="green")
+  plot(t,y,type = "b",main = "graphe des états " ,pch=5,xlim=c(0,n),ylim=c(0,length(mu)+1),xlab = "temps" ,ylab = "états",col="green")
   par(mfrow = c(2,1))
   return(y)
 }  
